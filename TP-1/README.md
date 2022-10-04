d5320b9dd6fd30a61f13d9a09ec180147d3d901f
Yannis Van Achter <yannis.van.achter@gmail.com>
2022-10-04 12:27:45 +0200

    exercices groupe
    avec la collaboration de
    @Ska02 et @julien8vp

---
+++ TP-1/README.md
@@ -0,0 +1,5 @@
+# Team Work:
+after a long delibaration we decided to use the function: <br />
+- "scanf()" in the <a herf="./src/main.c">main.c program</a> in the <a href="./src"> scource repertory</a>
+- "getchar()" in the <a href="./src/main.c">mainc.c program</a> in the <a href="./src"> source repository</a>
+
--- TP-1/src/main.c
+++ TP-1/src/main.c
@@ -4,15 +4,37 @@
 #include <unistd.h>
 #include <string.h>
 
-/** 
- * Write the funcitons here
-*/
+char buffer[1024];
 
-int main(int argc, char argv[])
+void handler(int signum);
+
+int main(void)
 {
-    /** 
-     * Write the code here
-     */
-
+    signal(SIGALRM, handler);
+    while (1) // infinity loop
+    {
+        alarm(5);
+        printf("Enter char:\n");
+        char string[1024];
+        scanf("%s", string);
+        strcat(buffer, string);
+    }
     return EXIT_SUCCESS;
+}
+void handler(int signum)
+{
+    if (signum == SIGALRM)
+    {
+        int lenght = strlen(buffer);
+        printf("Lenght: %d\n", lenght);
+        if (lenght == 0)
+        {
+            exit(EXIT_SUCCESS);
+        }
+        printf("The input was:\n");
+        printf("%s",buffer);
+        printf("\n");
+        memset(buffer,0,lenght);
+        alarm(5);
+    }
 }
