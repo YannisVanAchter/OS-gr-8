f75427a01aa4cb6d6fc50e4309426e05b469d282
Yannis Van Achter <yannis.van.achter@gmail.com>
2022-10-27 10:30:03 +0200

    Update main.c
    Add rotate
    + update struct node et circular

--- TP-2/src/main.c
+++ TP-2/src/main.c
@@ -1,27 +1,26 @@
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
 struct node 
 {
-    unsigned int* previous; // pointer to previous node
+    struct node* previous; // pointer to previous node
     signed int data; // current value of this node
-    unsigned int* next; // pointer to next node
-}
+    struct node* next; // pointer to next node
+};
 
 struct circular 
 {
-    struct node* curentnode; // curent node
-    struct node* lastnode; // node where after wich we add the next value
-    struct node* firstnode; // first node of the circular
+    struct node* lastNode; // node where after wich we add the next value
+    struct node* firstNode; // first node of the circular
     int head; // curent lecture cursor
-    int tail; // curent size of the circular
+    unsigned int size; // curent size of the circular
 };
 
 struct circular* mkCircular()
 {
     /** 
-     * Return the pointer to adress of the wielding circular
+     * Return the pointer of the adress of the circular
      * 
      * @return the address of the pointer where the circular is starting
     */
@@ -44,16 +43,37 @@ void deleteCircular(unsigned int* cycle)
      */
 }
 
+struct node* rotate(struct circular* cycle, int offset)
+{
+    struct node* nodeToReturn;
+    struct node* startingPoint = cycle->currentNode;
+    do
+    {
+        nodeToReturn = *((cycle->currentNode)->next);
+        cycle->head++;
+    
+    } while ((cycle->currentNode)->data % 2 == offset && nodeToReturn != startingPoint);
+
+    if (nodeToReturn == startingPoint && (cycle->currentNode)->data % 2 != offset)
+    {
+        char type = (offset == 0) ? 'odd' : 'even';
+        printf("The program did not find %s in the circular", type);
+    }
+
+    return nodeToReturn;
+}
+
 struct node* rotateToEven(struct circular* cycle)
 {
     /**
      * 
      */
+    return rotate(cycle, 1);
 }
 
 struct node* rotateToOdd(struct circular* cycle)
 {
-
+    return rotate(cycle, 0);
 }
 
 void insert(unsigned int* cycle, signed int elt)
@@ -61,7 +81,7 @@ void insert(unsigned int* cycle, signed int elt)
     /** 
      * Add at the next address the node specified in the circular
      * @param cycle the address of the circular where insert the value
-     * @param value  the value to insert
+     * @param elt  the value to insert
      */
 }
 
