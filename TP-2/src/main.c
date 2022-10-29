ecfa9a5f1d6e9b4b52302b4ca04168a176daaaac
Yannis Van Achter <yannis.van.achter@gmail.com>
2022-10-29 12:22:36 +0200

    Update main.c
    Ajout de commentaire pour la compréhension + update du code fonction des commentaire de Youlan quand il est passé jeudi

--- TP-2/src/main.c
+++ TP-2/src/main.c
@@ -29,7 +29,7 @@ struct circular* mkCircular()
 signed int extract(struct circular* cycle )
 {
     /** 
-     * return the value containd at curent node
+     * return the value containd at curent node and pass to the next
      * @param cycle pointer to the circular
      * @return the data contained at the current node
     */
@@ -45,19 +45,38 @@ void deleteCircular(unsigned int* cycle)
 
 struct node* rotate(struct circular* cycle, int offset)
 {
-    struct node* nodeToReturn;
-    struct node* startingPoint = cycle->currentNode;
-    do
-    {
-        nodeToReturn = *((cycle->currentNode)->next);
-        cycle->head++;
-    
-    } while ((cycle->currentNode)->data % 2 == offset && nodeToReturn != startingPoint);
+    /**
+     * Apply a rotation to until we met the first odd or even value in the circular
+     * 
+     * @param cycle the address of the circular
+     * @param offset 0 if we want odd value, 1 if we want even value
+     * @return the address of the node wich contain an odd value 
+     * If there is no odd value in the circular it return NULL
+     */
+     // check the offset to make sure it will respond as attend
+     if (offset != 0 || offset != 1)
+     {
+        printf("The specified offset is invalid, make sure this is 0 or 1");
+        exit(EXIT_FAILURE);
+     }
 
-    if (nodeToReturn == startingPoint && (cycle->currentNode)->data % 2 != offset)
+    // allow memory for usefull variable
+    struct node* nodeToReturn = malloc(sizeof(struct node));
+    struct node* startingPoint = cycle->firstNode;
+
+    do // loop until we met a node where the data corresponds to attend
     {
-        char type = (offset == 0) ? 'odd' : 'even';
+        nodeToReturn = ((cycle->firstNode)->next);
+        cycle->head++;
+        cycle->firstNode = nodeToReturn;
+    } while ((cycle->firstNode)->data % 2 == offset && nodeToReturn != startingPoint);
+
+    // If the circular does not contain attend we print message to inform and return null
+    if (nodeToReturn == startingPoint && (cycle->firstNode)->data % 2 != offset)
+    {
+        char type[5] = (offset == 0) ? "odd\0" : "even\0";
         printf("The program did not find %s in the circular", type);
+        return NULL;
     }
 
     return nodeToReturn;
@@ -66,20 +85,31 @@ struct node* rotate(struct circular* cycle, int offset)
 struct node* rotateToEven(struct circular* cycle)
 {
     /**
+     * Apply a rotation to until we met the first even value in the circular
      * 
+     * @param cycle the address of the circular
+     * @return the address of the node wich contain an even value 
+     * If there is no even value in the circular it return NULL
      */
     return rotate(cycle, 1);
 }
 
 struct node* rotateToOdd(struct circular* cycle)
 {
+    /**
+     * Apply a rotation to until we met the first odd value in the circular
+     * 
+     * @param cycle the address of the circular
+     * @return the address of the node wich contain an odd value 
+     * If there is no odd value in the circular it return NULL
+     */
     return rotate(cycle, 0);
 }
 
 void insert(unsigned int* cycle, signed int elt)
 {
     /** 
-     * Add at the next address the node specified in the circular
+     * Add at the node specified in the circular at the end
      * @param cycle the address of the circular where insert the value
      * @param elt  the value to insert
      */
@@ -88,6 +118,9 @@ void insert(unsigned int* cycle, signed int elt)
 void delete(unsigned int* cycle)
 {
     /**
+     * Will delete the current value in the circular and pass to the next one
+     * When delete the memory is freed
+     * 
      * @param cycle the address of the circular where remove the value
      * 
      * If this is the last value in the circular, we delete the circular
@@ -97,6 +130,8 @@ void delete(unsigned int* cycle)
 int main(int argc, char* argv[])
 {
     /**
+     * Main function where we write the test of the structure and the function
+     * 
      * @param argc the number of arguments (int)
      * @param argv pointer to the arguments passed in command line (char [])
      * @return 0 on success
