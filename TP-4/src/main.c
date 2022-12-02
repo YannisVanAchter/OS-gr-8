9c3f2d98e3cafaaf26e2e63819b1509de1a43789
Yannis Van Achter <yannis.van.achter@gmail.com>
2022-12-02 21:30:04 +0100

    Update main.c

--- TP-4/src/main.c
+++ TP-4/src/main.c
@@ -1,17 +1,17 @@
-#include<stdio.h> // printf
+#include<stdio.h> // printf()
 #include<stdlib.h> // EXIT_SUCCESS
-#include<pthread.h> // pthread_t, pthread_create(), pthread_join()
-#include<semaphore.h> // sem_t, sem_init(), sem_wait(), sem_post()
-#include<unistd.h> // sleep
+#include<pthread.h> // pthread_t, pthread_create(), pthread_mutex_trylock(), pthread_mutex_unlock(), pthread_mutex_init, pthread_mutex_t
+#include<unistd.h> // sleep()
 
-#define NB_PHILOSPHE 5
+#ifndef NB_PHILOSOPHE
+#define NB_PHILOSOPHE 5
 
-#define DROITE(i) (( i ) % NB_PHILOSPHE)
-#define GAUCHE(i) ((i + 1) % NB_PHILOSPHE)
+#define DROITE(i) (( i ) % NB_PHILOSOPHE)
+#define GAUCHE(i) ((i + 1) % NB_PHILOSOPHE)
 #define false 0
 #define true 1
 
-pthread_mutex_t baguettes[NB_PHILOSPHE];
+pthread_mutex_t baguettes[NB_PHILOSOPHE];
 
 void manger(int phil)
 {
@@ -22,25 +22,28 @@ void manger(int phil)
 
 int tenter(int phil)
 {
-    printf("Le philosophe n° %d essaye de prendre les baguettes\n", phil);
+    printf("Le philosophe n %d essaye de prendre les baguettes\n", phil);
 
     // Try take the chopsticks
 	int right = pthread_mutex_trylock(&baguettes[DROITE(phil)]);
 	int left = pthread_mutex_trylock(&baguettes[GAUCHE(phil)]);
     
-	if (left == 0 && right == 0)
-	{
-    	printf("A récupérer les baguettess\n");
+	if (left == 0 && right == 0) 
+	{ // if philosopher has chopsticks
+    	printf("%d a récupéré les baguettess\n", phil);
 		return 1;
 	}
 	if (left != 0)
-	{
+	{ // if philosopher didn't get the left one
 		pthread_mutex_unlock(&baguettes[GAUCHE(phil)]);
 	}
 	if (right != 0)
-	{
+	{ // if philosopher didn't get the right one
 		pthread_mutex_unlock(&baguettes[DROITE(phil)]);
 	}
+    
+	printf("%d n'a pas récupéré les baguettess\n", phil);
+
 	return 0;
 }
 
@@ -62,15 +65,13 @@ void * philosophe(void * num)
 {
 	int phil = *(int *)num;
 	printf("Je suis le philosophe %d\n",phil);
-	int s = 0;
+	int have_chopsticks = 0;
 
 	while(true) 
-	{
-		printf("Je suis le philosophe %d\n",phil);
-		
-		s = tenter(phil);
-		if (s)
-		{
+	{		
+		have_chopsticks = tenter(phil);
+		if (have_chopsticks)
+		{ // if philosopher has chopsticks he can eat
 			manger(phil);
 			libere(phil);
 		}
@@ -78,22 +79,26 @@ void * philosophe(void * num)
 	}
 }
 
-int main()
+int main(int *argc, int *argv[])
 {
-	int i,a[NB_PHILOSPHE];
-	pthread_t tid[NB_PHILOSPHE];
+	int i,a[NB_PHILOSOPHE];
+	pthread_t pid[NB_PHILOSOPHE];
 
-	for (i=0; i<NB_PHILOSPHE; i++)
-	{
-
+	// set each chopstrics as a mutex
+	for (i=0; i<NB_PHILOSOPHE; i++)
 		pthread_mutex_init(&baguettes[i], NULL);
-	}
 		
-	for(i=0;i<NB_PHILOSPHE;i++)
+	// generate thread for each philosopher
+	for(i=0;i<NB_PHILOSOPHE;i++)
 	{
 		a[i]=i;
-		pthread_create(&tid[i], NULL, philosophe, (void *)&a[i]);
+		pthread_create(&pid[i], NULL, philosophe, (void *)&a[i]);
 	}
-	for(i=0;i<NB_PHILOSPHE;i++)
-		pthread_join(tid[i], NULL);
+
+	for (i = 0; i<NB_PHILOSOPHE; i++)
+		pthread_join(pid[i], NULL);
+	
+	return EXIT_SUCCESS;
 }
+
+#endif
